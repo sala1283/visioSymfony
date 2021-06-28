71da94ab056dcf06215d7870423cf0b69e4059ac
salah <hamidi.salah@gmail.com>
2021-06-29 01:42:47 +0200

    Front-end / Installer webpack

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+let btnContact = document.querySelector('#contact');
+btnContact.addEventListener('click', function(){
+    let form = document.querySelector('.form-contact');
+    form.classList.remove('hidden');
+    form.classList.add('visible');
+    this.classList.add('hidden');
+})
